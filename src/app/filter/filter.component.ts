d170bfcb7f156f6aeaf9a315c9f06b1e806ddf22
BHUPESH <vipul.chikara.6002@gmail.com>
2019-10-22 13:29:29 +0530

    search filter updated

--- src/app/filter/filter.component.ts
+++ src/app/filter/filter.component.ts
@@ -22,16 +22,29 @@ export class FilterComponent implements OnInit {
   }
 
   filter(query: string) {
-    console.log(query)
-    console.log(this.products)
+    // console.log(query)
+    // console.log(this.products)
     if(this.products && this.filteredProducts){
-    let q = query.toLowerCase();
-    console.log(this.filteredProducts)
+      let q = query.toLowerCase();
+      var words = q.split(' ')
+    // console.log(this.filteredProducts)
      this.products = this.filteredProducts;
-     console.log(this.products)
+    //  console.log(this.products)
     this.products = (query) ?
-      this.products.filter(p => p.title.toLowerCase().includes(q)) : this.filteredProducts;
-    console.log(this.products)
+      this.products.filter(p => {
+        let toFilter = false;
+        words.forEach(word=>{
+          if(p.title.toLowerCase().includes(word)) toFilter=true;
+        })
+        return toFilter;
+        // p.title.toLowerCase().includes(q)
+        // for(let i in words){
+        // return  p.title.toLowerCase().includes(words[i]) 
+
+        // }
+      }
+      ) : this.filteredProducts;
+    // console.log(this.products)
       this.filtering.emit(this.products)
   }
   }
